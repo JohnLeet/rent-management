528a6cd740f12acfbe204ee2a00b902061d581bb
Leet <vasiliy.l@thrive.io>
2026-06-11 13:34:59 +0300

    feat(frontend): added struture for frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@app': path.resolve(__dirname, './src/app'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@modules': path.resolve(__dirname, './src/modules'),
+            '@layouts': path.resolve(__dirname, './src/layouts'),
+            '@shared': path.resolve(__dirname, './src/shared'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+        },
+    },
+});
